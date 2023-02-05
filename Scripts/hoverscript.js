c7949ab5b4fb8f6e811dbdbe784fba965d5d5d68
Girafae <girafaeone@gmail.com>
2023-02-05 16:23:29 +0000

    Update
    Versão 0.0.1

---
+++ Scripts/hoverscript.js
@@ -0,0 +1,18 @@
+function Infos(Infs)
+{
+    
+ var infosgui = document.getElementById("Infos")
+    if (Infs == 'HTML')
+    {
+    
+infosgui.innerHTML = "HTML é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores. A tecnologia é fruto da junção entre os padrões HyTime e SGML. HyTime é um padrão para a representação estruturada de hipermídia <br> fonte:  <a href='https://pt.wikipedia.org/wiki/HTML'>Wikipedia</a> "  
+
+    }
+if (Infs == "CSS")
+{
+
+    infosgui.innerHTML = "CSS3 é a terceira mais nova versão das famosas Cascading Style Sheets, onde se define estilos para seu projeto web.Com efeitos de transição, imagem,imagem de fundo/Background e outros, que dão um estilo novo e elegante a seus projetos web.Ou em todos os aspectos de design do layout da página. <br> fonte:  <a href='https://pt.wikipedia.org/wiki/CSS3 '>Wikipedia</a>  "
+    
+}
+
+}
